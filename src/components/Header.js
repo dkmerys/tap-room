30546b1405f25eb4ea50bd87c62c5b18ca630206
Daniel Merys <dkmerys@gmail.com>
2020-07-03 09:26:51 -0700

    add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Growlers Taproom</h1>
+      <h3>Keg Management</h3>
+    </React.Fragment>
+  );
+}
+
+export default Header;
