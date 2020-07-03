9cc878de1fc3dc3a822c7bc7dbe0be82741fc0fa
Daniel Merys <dkmerys@gmail.com>
2020-07-03 16:32:51 -0700

    add minimal styling

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,17 @@ import KegControl from './KegControl';
 
 
 function App() {
+  const appStyles = {
+    backgroundColor: '#ffffff',
+    backgroundImage: 'linear-gradient(315deg, #ffffff 0%, #274060 74%)'
+  }
   return (
+    <div style={appStyles}>
     <React.Fragment>
       <Header />
       <KegControl />
     </React.Fragment>
+    </div>
   );
 }
 
