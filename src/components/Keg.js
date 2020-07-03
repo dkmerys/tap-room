ea92d2e26ef05a7217108d5274be704026126ab0
Daniel Merys <dkmerys@gmail.com>
2020-07-03 11:07:48 -0700

    pass whenKegClick method into Keg component as prop

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-funtion Keg(props) {
+function Keg(props) {
   return (
     <React.Fragment>
-      <h3>{props.name}</h3>
-      <h4>{props.brewery}</h4>
-      <h5>{props.description}</h5>
-      <h4>{props.abv}</h4>
-      <h4>{props.price}</h4>
-      <h5>{props.quantity}</h5>
+      <div onClick = {() => props.whenKegClicked(props.id)}>
+        <h3>{props.name}</h3>
+        <h4>{props.brewery}</h4>
+        <h5>{props.description}</h5>
+        <h4>{props.abv}</h4>
+        <h4>{props.price}</h4>
+        <h5>{props.quantity}</h5>
+      </div>
     </React.Fragment>
   );
 }
@@ -22,6 +24,7 @@ Keg.propTypes = {
   price: PropTypes.string,
   quantity: PropTypes.number,
   id: PropTypes.string,
+  whenKegClicked: PropTypes.func
 }
 
 export default Keg;
